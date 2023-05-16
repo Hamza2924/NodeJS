8adc1acfea965df5113e7e211ba0dacd3c94fe18
M Hamza Shaukat <mhamzashaukat786@gmail.com>
2023-05-16 09:52:35 +0500

    Lodash Practice

---
+++ Lodash/lodash.js
@@ -0,0 +1,64 @@
+const _ = require ("lodash");
+
+const Fruits =[
+
+    {"id":1,"name":"Apple"},
+    {"id":2,"name":"Mango"},
+    {"id":3,"name":"Orange"}
+
+]
+
+
+// //Use 
+// const keyedarr= _.keyBy(Fruits,"id");
+// console.log(keyedarr); 
+
+// var objects = [{ 'x': 4 ,"Name":"ALi","Class":8}, { 'x': 5 ,"Name":"Saqib","Class":3}];
+ 
+// let B=_.sortedLastIndexBy(objects, { "Class":8 }, function(o) { return o.x; });
+// console.log("B",B);
+
+
+///MMAx
+
+// let arr =[2,3,4,8,4,6,9,9,null,'',8,0,9];
+let arr =[2,3,4,8,4,6,9,9];
+   
+ //console.log(_.max(arr));
+
+
+ //Min
+//  console.log(_.min(arr));
+
+
+ //String
+//  let a="Sohail kia hal ha"
+//  console.log(_.isString(1));
+
+//Concate
+// console.log(_.concat(arr,11,14,15));
+
+//SPLIT
+// console.log(_.chunk(arr,2));
+
+//Compact
+// console.log(_.compact(arr));
+
+//SEPERATOR
+// console.log(_.join(arr,"!"));
+
+// Last Index value
+// console.log(_.lastIndexOf(Fruits));
+//console.log(_.last(Fruits));
+
+//NTH
+// console.log(_.nth(arr,0));
+
+// PULL ITEMS
+
+// console.log(_.pull(arr,2,3,4,9));
+// console.log(_.pull(Fruits,Fruits.name="Apple"));
+// console.log(_.pull(Fruits,1));
+
+
+
